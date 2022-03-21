514635e979715e416aa51cdf54206d26a86e5de4
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2022-03-21 19:19:49 +0530

    Add Ec2

---
+++ ec2.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "myec2" {
+  ami           = "ami-054965c6cd7c6e462"
+  instance_type = "t2.micro"
+}
---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIATCJUZZOCVBNZC47V"
+  secret_key = "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"
+
+}
