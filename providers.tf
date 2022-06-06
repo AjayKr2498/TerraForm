66bce230f8bad17e69e51ce31659b3ccec0362fa
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2022-06-06 11:31:38 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIATCJUZZOCYOH4OH4L"
-  secret_key = "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"
+  access_key = "AKIATCJUZZOCYOH4OH4L2498"
+  secret_key = "yoLJ0b5HRgZ8Rds0yCWYzYDYwfZ3o49M8LvHAHTjajay"
 
 }
