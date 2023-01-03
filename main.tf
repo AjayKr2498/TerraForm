251509f090e32bf3c2420e34a6743c75baa99606
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2023-01-03 17:47:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-# provider "aws" {
-#     region = "us-east-2"
-# }
+provider "aws" {
+    region = "us-east-2"
+}
 
-# resource "aws_s3_bucket" "terraform-state" {
-#     bucket = "demo-ajaykumar240398-terraform"
-#     #basically it prevents accidental deletion of s3 bucket.
-#     lifecycle {
-#         prevent_destroy = true
+resource "aws_s3_bucket" "terraform-state" {
+    bucket = "demo-ajaykumar240398-terraform"
+    #basically it prevents accidental deletion of s3 bucket.
+    lifecycle {
+        prevent_destroy = true
         
-#     }
-#     #Enable versioning to see full revision of our state files
-#     versioning {
-#         enabled = true
-#     }
-# #enable server side encryption(SSE)
-# server_side_encryption_configuration {
-#     rule {
-#         apply_server_side_encryption_by_default {
-#             sse_algorithm = "AES256"
-#         }
-#     }
-#   }
-# }
+    }
+    #Enable versioning to see full revision of our state files
+    versioning {
+        enabled = true
+    }
+#enable server side encryption(SSE)
+server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+  }
+}
 
 # resource "aws_dynamodb_table" "terraform_locks" {
 #     hash_key = "LockID"
