47205769d6204dbff1fa5fbe20715e885164ad3e
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2023-01-04 19:05:48 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
      region = "us-east-2"
  }
 
-resource "aws_instance" "demo" {
+resource "aws_instance"  {
   ami           = "ami-0a606d8395a538502"
   instance_type = "t2.micro"
 }
