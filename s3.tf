01d242f6fe3f7a6f947def03d4bae57bd5b18a32
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2023-01-05 12:40:30 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
  }
 
 resource "aws_s3_bucket" "s3" {
-      bucket = "000-test-000-demo"
+      bucket = "000-test-000-test"
 #basically it prevents accidental deletion of s3 bucket.
       lifecycle {
           prevent_destroy = true
