4e91c9bc4a2e0211cae706c347f9274e8141883e
AjayKr2498 <101340597+AjayKr2498@users.noreply.github.com>
2023-02-14 11:14:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "myec2" {
-  ami           = "ami-054965c6cd7c6e462"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "myec2" {
+#   ami           = "ami-054965c6cd7c6e462"
+#   instance_type = "t2.micro"
+# }
